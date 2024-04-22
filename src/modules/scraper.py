72b8bd844cabf19e0ebeeae737eef20eb278eba5
Ahmed Hamdy Kotp <160382377+AhmedHamdyKotp@users.noreply.github.com>
2024-04-22 14:38:25 +0200

    Update scraper.py

--- src/modules/scraper.py
+++ src/modules/scraper.py
@@ -1,1 +1,23 @@
+import serpapi as se
+import sys
+from serpapi import GoogleSearch
+import requests as re
+import os
+from dotenv  import load_dotenv
+env_API = 'data/.env'
+load_dotenv(dotenv_path=env_API)
+apik = os.getenv('S_API')
+import analyzer
+def organizing(or_result):
+    Data = []
+    for item in or_result :
+        url = item['link']
+        title = item['title']
+        snippet = item['snippet']
+        data={"url":url,"title":title,"snippet":snippet}
+        Data.append(data)
+    analyzer.extract_profession(Data)
+    
+
+
 
