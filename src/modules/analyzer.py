ffe1128988c350126b16167dba8b242b52bb81fb
Ahmed Hamdy Kotp <160382377+AhmedHamdyKotp@users.noreply.github.com>
2024-04-22 14:37:59 +0200

    Update analyzer.py

--- src/modules/analyzer.py
+++ src/modules/analyzer.py
@@ -1,1 +1,24 @@
+import sys
+sys.path.append('src/utils')
+sys.path.append('src/modules')
+
+import scraper as sc
+import constants 
+
+def extract_profession(data):
+    profession = constants.get_professions()    
+    profession_found = {}
+
+    for item in data:
+        for i in profession:
+            if i.lower() in item['title'].lower() or i.lower() in item['snippet'].lower():
+                profession_found[i] = profession_found.get(i, []) + [item['url']]
+
+    for profession, urls in profession_found.items():
+        print(f"Profession: {profession}")
+        for url in urls:
+            print(f"URL: {url}")
+        print("-------------------------")
+    return profession_found
+
 
