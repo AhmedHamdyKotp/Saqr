ba1862988f540493a5933fd8abcc0aed13f029e9
Ahmed Hamdy Kotp <160382377+AhmedHamdyKotp@users.noreply.github.com>
2024-04-23 18:16:36 +0200

    Update main.py

--- src/main.py
+++ src/main.py
@@ -4,23 +4,26 @@ import sys
 sys.path.append("src/modules")  
 import scraper
 
-def user_input(api_key: str, search_term: str) -> None:
+def user_input(api_key: str) -> None:
+    while True:
+        print("Enter a word to look for (or 'exit' to quit):")
+        search_term = input()
+        if search_term.lower() == 'exit':
+            break
 
-    params = {
-        "q": search_term,
-        'engine': 'google',
-        'hl':'en',
-        'api_key': api_key,
-        'num' : 100,
-    }
+        params = {
+            "q": search_term,
+            'engine': 'google',
+            'hl':'en',
+            'api_key': api_key,
+            'num' : 100,
+        }
 
-    search = se.GoogleSearch(params)
-    result = search.get_dict()
-    scraper.organizing(result['organic_results'])
+        search = se.GoogleSearch(params)
+        result = search.get_dict()
+        scraper.organizing(result['organic_results'])
 
 if __name__ == "__main__":
     print("Enter your SerpAPI key: ")
     api_key = input()
-    print("Enter a word to look for:")
-    search_term = input()
-    user_input(api_key, search_term)
+    user_input(api_key)
